db18be171cad1f1e16413cbe7bc815745e92804f
vedjangid19 <vedprakashjangid.jpr@gmail.com>
2022-06-24 00:42:48 +0530

    testing ci cd.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-	return 'Successfully setup CI/CD pipeline on heroku.'
+	return 'Successfully setup CI/CD pipeline on heroku1.'
 
 
 if __name__ == '__main__':
