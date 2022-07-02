025a81c5c9e25a4215f4b8358644f3dd9d4a7ef5
vedjangid19 <vedprakashjangid.jpr@gmail.com>
2022-07-03 04:15:08 +0530

    validation start.

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -16,12 +16,18 @@ class HousingException(Exception):
         
         _,_,exec_tb = error_detail.exc_info()
         
-        line_number = exec_tb.tb_frame.f_lineno
+        exception_block_line_number = exec_tb.tb_frame.f_lineno
+        try_block_line_number = exec_tb.tb_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
         
         error_message = f"""
-        Error occured in script : [{file_name}] at line number : [{line_number}] 
-        error message : [{error_message}]
+        
+        Error occured in script : 
+        [ {file_name} ] at 
+        try block line number : [{try_block_line_number}] and exception block line number : [{exception_block_line_number}]
+        error message :
+        [{error_message}]
+        
         """
         return error_message
     
