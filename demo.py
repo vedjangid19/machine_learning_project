6bc4bd76d993e2f65a4a98dd3c24feb07c7e1f67
vedjangid19 <vedprakashjangid.jpr@gmail.com>
2022-07-18 02:34:40 +0530

    Data Transformation done.

--- demo.py
+++ demo.py
@@ -8,7 +8,7 @@ def main():
     try:
         pipeline = Pipeline()
         pipeline.run_pipeline()
-        # data_validation_config = Configuration().get_data_validation_config()
+        # data_validation_config = Configuration().get_data_transformation_config()
         # print(data_validation_config)
     except Exception as e:
         raise HousingException(e,sys) from e
